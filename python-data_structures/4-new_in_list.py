5ed3859e9f8661d122788a4be98900f547507ad0
Jeffrey Dauda <j.dauda@alustudent.com>
2022-10-30 17:50:31 -0700

    Replace in a copy

---
+++ python-data_structures/4-new_in_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+define new_in_list(my_list, idx, element):
+    length = len(my_list)
+    if idx <0:
+        newlist = my_list[:]
+        return newlist
+    elif idx >= length:
+        newlist_2 = my_list[:]
+        return newlist_2
+    else:
+        newlist_3 = my_list[:]
+        newlist_3[idx] = element
+        return (newlist_3)
