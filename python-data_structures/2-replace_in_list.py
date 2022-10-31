6c296ec4476f30129920321e4caaf8ccbdbd4a68
Jeffrey Dauda <j.dauda@alustudent.com>
2022-10-30 17:28:36 -0700

    Replace element

--- python-data_structures/2-replace_in_list.py
+++ python-data_structures/2-replace_in_list.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 def replace_in_list(my_list, idx, element):
     if idx < 0:
-        return (my_list)
-    length = len(my_list)
-    if idx > len - 1:
-        return (my_list)
-
-    my_list[idx] = element
-    return (my_list)
+        return my_list
+    elif idx >= len(my_list):
+        return my_list
+    else:
+        my_list[idx] = element
+        return my_list
