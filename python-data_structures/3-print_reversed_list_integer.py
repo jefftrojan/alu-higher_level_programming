0042f04a0678cc80551745ae8774d9c4201cc8f3
Jeffrey Dauda <j.dauda@alustudent.com>
2022-10-30 17:36:54 -0700

    Print int in reverse

---
+++ python-data_structures/3-print_reversed_list_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def print_reversed_list_integer(my_list=[]):
+    if not my_list:
+        pass 
+    else:
+        my_list.reverse()
+        length = len(my_list)
+        for i in range(length):
+            print("{:d}".format(my_list[i]))
+
