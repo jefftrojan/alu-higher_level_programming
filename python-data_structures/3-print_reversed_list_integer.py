306387be06ae30d1be24b5a3c70e4f3e603872df
Jeffrey Dauda <j.dauda@alustudent.com>
2022-10-30 17:39:00 -0700

    Print int in reverse

--- python-data_structures/3-print_reversed_list_integer.py
+++ python-data_structures/3-print_reversed_list_integer.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
     if not my_list:
-        pass 
+        pass
     else:
         my_list.reverse()
         length = len(my_list)
