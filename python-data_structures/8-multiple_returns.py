1773688cc0cf1fca236d63b38ccc4361dc1a32b3
Jeffrey Dauda <j.dauda@alustudent.com>
2022-11-03 14:58:46 -0700

    Multiple returns

---
+++ python-data_structures/8-multiple_returns.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def multiple_returns(sentence):
+    sentence_len = len(sentence)
+    if sentence_len == 0:
+        new_tuple = (sentence_len, None)
+    else:
+        new_tuple = (sentence_len[0])
+    return new_tuple
