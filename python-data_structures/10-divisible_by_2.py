6c3536bf66ff234b42d188b25b4cb97aae489511
Jeffrey Dauda <j.dauda@alustudent.com>
2022-11-03 15:32:58 -0700

    Multiples of 2

--- python-data_structures/10-divisible_by_2.py
+++ python-data_structures/10-divisible_by_2.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
     list_len = len(my_list)
-    for i in list_len:
+    div_checker = []
+    for i in range(list_len):
         if list_len[i] % 2 == 0:
-            while list_len < 0:
-                new_list = list_len[True]
-                return new_list
-            else:
-                new_list = list_len[False]
-                return new_list
+            div_checker.append(True)
+        else:
+            div_checker.append(False)
+    return div_checker
