a4d59dc3ff176f9126e9358311926407c0e5df56
Jeffrey Dauda <j.dauda@alustudent.com>
2022-11-03 15:09:50 -0700

    Max int

---
+++ python-data_structures/9-max_integer.py
@@ -0,0 +1,11 @@
+#!/bin/usr/python3
+def max_integer(my_list=[]):
+    list_len = len(my_list)
+    if list_len <= 0:
+        return (None)
+    max_int = my_list[0]
+
+    for i in range(1, length):
+        if my_list[i] > max_int:
+            max_int = my_list[i]
+    return max_int
