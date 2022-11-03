1ab3c142e9c5da2e4f8cc71581e2468613607d96
Jeffrey Dauda <j.dauda@alustudent.com>
2022-11-03 16:25:24 -0700

    Fix | multiple returns-8

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -4,5 +4,5 @@ def multiple_returns(sentence):
     if sentence_len == 0:
         new_tuple = (sentence_len, None)
     else:
-        new_tuple = (sentence_len[0])
+        new_tuple = (sentence_len, sentence[0])
     return new_tuple
