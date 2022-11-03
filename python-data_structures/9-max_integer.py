eb3afe3446816535d299e2d847a03f74be0b8ae4
Jeffrey Dauda <j.dauda@alustudent.com>
2022-11-03 16:19:18 -0700

    max int fix

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -5,7 +5,7 @@ def max_integer(my_list=[]):
         return (None)
     max_int = my_list[0]
 
-    for i in range(1, length):
+    for i in range(1, list_len):
         if my_list[i] > max_int:
             max_int = my_list[i]
     return max_int
