68b4949e7d6338abad793bf0d654475a733b5100
Jeffrey Dauda <j.dauda@alustudent.com>
2022-11-03 15:52:36 -0700

    delet at

--- python-data_structures/11-delete_at.py
+++ python-data_structures/11-delete_at.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def delete_at(my_list=[], idx=0):
-    if idx <= 0:
+    if idx <= 0 or idx >= list_len:
         return my_list
     list_len = len(my_list)
     for i in range(list_len):
