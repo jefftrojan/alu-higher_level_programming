93499e93ef460738648fbe27de62e83e5f4737cb
Jeff Dauda <j.dauda@alustudent.com>
2022-11-11 21:28:55 +0000

    Some fix

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -2,10 +2,10 @@
 """ A class that defines a square by its size
 """
 
+
 class Square:
     """ A class that defines a square by its size
 """
-   
     def __init__(self, size=0):
         """ Method to initialize the square object
         """
