9e86dc73e16c2ec142d656c0c0755a1edf36dfc2
Jeff Dauda <j.dauda@alustudent.com>
2022-11-11 21:16:38 +0000

    Some fix

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -1,17 +1,16 @@
 #!/usr/bin/python3
 """ A class that defines a square by its size
     """
-    
+
+
 class Square:
     """ A class that defines a square by its size
     """
-    
-    
+
+
     def __init__(self, size=0):
         """ Method to initialize the square object
         """
-
-
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         elif size < 0:
