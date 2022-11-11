3563f488bc2b35e234890a1cb0d9504fb7b6cedd
Jeff Trojan <jefdauda@gmail.com>
2022-11-11 22:45:26 +0200

    added task 1

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -4,9 +4,9 @@
 
 class Square:
     """Class Square that defines a square object"""
-    
 
-    def __init__(self, size):
+
+   def __init__(self, size):
         """Initialize method that stores the size of the square
 
         Args:
