a0728823a22a59e30405645d9eecd09afe105ade
Jeff Trojan <jefdauda@gmail.com>
2022-11-18 12:52:50 +0200

    Added doc for 7

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -8,6 +8,11 @@ import sys
 import os.path
 
 
+""" Module that adds all arguments to a Python list, and then
+save them to a file
+"""
+
+
 save_file = __import__('7-save_to_json_file').save_to_json_file
 load_file = __import__('8-load_from_json_file').load_from_json_file
 
