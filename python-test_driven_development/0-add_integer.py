1e1387ea728fa013bf9b290fd64012fc0cbb9dd8
Jeffrey Dauda <j.dauda@alustudent.com>
2022-11-28 02:16:53 -0800

    0-ADD_INT : TESTS/

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -4,13 +4,10 @@
 
 def add_integer(a, b=98):
     """ Func that SUm 2 integers and or float otherwise throws an exception """
-
-
     if not isinstance(a, int) and not isinstance(a, float):
         raise TypeError("a must be an integer")
     if not isinstance(b, int) and not isinstance(b, float):
         raise TypeError("b must be an integer")
-
     a = int(a)
     b - int(b)
 
