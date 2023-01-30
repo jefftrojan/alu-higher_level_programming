5fbf553dcc60392cbbb4df8ff312e43c618593a9
Jeff Trojan <j.dauda@alustudent.com>
2023-01-30 19:37:55 +0200

    To integer

---
+++ javascript-warm_up/5-to_integer.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+const arg = process.argv;
+num_check = Number.isInteger(arg)
+
+if (num_check === true) {
+  console.log('My number:' + arg[0])
+} else {
+  console.log('Not a number')
+}
