19b5b5e2473113cf8cdac468a79f67f0cb81d651
Jeff Trojan <j.dauda@alustudent.com>
2023-01-30 19:49:00 +0200

    5-to_int

--- javascript-warm_up/5-to_integer.js
+++ javascript-warm_up/5-to_integer.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 
-const arg = process.argv;
-let num_check = Number.isInteger(arg);
+const arg = process.argv[2];
+let Num_check = Number.isInteger(arg);
 
-if (num_check === true) {
+if (Num_check === true) {
   console.log("My number:" + arg[0]);
 } else {
   console.log("Not a number");
