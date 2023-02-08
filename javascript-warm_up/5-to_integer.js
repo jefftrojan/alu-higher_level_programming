245f0718468064eb5bf0e33dbb090b2f3890dbed
Jeff Trojan <j.dauda@alustudent.com>
2023-02-08 23:55:46 +0200

    Fix for 5

--- javascript-warm_up/5-to_integer.js
+++ javascript-warm_up/5-to_integer.js
@@ -1,12 +1,7 @@
 #!/usr/bin/node
-
-const arg = process.argv[2];
-const Numcheck = Number.isInteger(arg);
-
-if (!Numcheck === true) {
-  console.log(`My number: ${arg}`);
-}
-
-if (!Numcheck === false) {
+const number = parseInt(process.argv[2]);
+if (Number.isNaN(number)) {
   console.log('Not a number');
+} else {
+  console.log('My number: ' + number);
 }
