1f893ed1323e7acd01f4dd1243f5640f6c1a7e27
Jeff Trojan <j.dauda@alustudent.com>
2023-02-09 04:23:44 +0200

    4 rectanglee

--- javascript_objects_scopes_closures/4-rectangle.js
+++ javascript_objects_scopes_closures/4-rectangle.js
@@ -23,4 +23,9 @@ module.exports = class Rectangle {
     this.width = this.height;
     this.height = currentState;
   }
+
+  double () {
+    this.width = this.width * 2;
+    this.height = this.height * 2;
+  }
 };
