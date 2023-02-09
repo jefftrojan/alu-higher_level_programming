90babf4c1b509c009849c527ad728515c6ecfcf9
Jeff Trojan <j.dauda@alustudent.com>
2023-02-09 04:14:23 +0200

    3 rectangle

---
+++ javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,22 @@
+#!/usr/bin/node
+
+const Rectangle = require('./1-rectangle');
+
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      let s = '';
+      for (let j = 0; j < this.width; j++) {
+        s += 'X';
+      }
+      console.log(s);
+    }
+  }
+};
